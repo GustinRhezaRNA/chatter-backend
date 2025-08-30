cf4611f01be8e6d2d379ee09f771cd58be9ba467
Gustin Rheza <rezarna4@gmail.com>
2025-08-30 14:39:43 +0700

    add backend subscriptions

--- src/chats/messages/messages.service.ts
+++ src/chats/messages/messages.service.ts
@@ -1,17 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ChatsRepository } from '../chats.repository';
 import { CreateMessageInput } from './dto/create-message.input';
 import { Message } from './entities/message.entity';
 import { Types } from 'mongoose';
 import { GetMessagesArgs } from './dto/get-messages.args';
+import { PUB_SUB } from 'src/common/constants/injection-token';
+import { PubSub } from 'graphql-subscriptions';
+import { MESSAGE_CREATED } from './constants/pubsub-trigger';
 
 @Injectable()
 export class MessagesService {
-  constructor(private readonly chatsRepository: ChatsRepository) {}
+  constructor(
+    private readonly chatsRepository: ChatsRepository,
+    @Inject(PUB_SUB) private readonly pubsub: PubSub,
+  ) {}
   async createMessage({ content, chatId }: CreateMessageInput, userId: string) {
     const message: Message = {
       content,
       userId,
+      chatId,
       _id: new Types.ObjectId(),
       createdAt: new Date(),
     };
@@ -24,6 +31,7 @@ export class MessagesService {
         $push: { messages: message },
       },
     );
+    await this.pubsub.publish(MESSAGE_CREATED, { messageCreated: message });
     return message;
   }
 
